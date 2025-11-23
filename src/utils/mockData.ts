1639124513b4b15583aa0382a9751f27a3a3d024
Leonardo de Melo <45073941+LeoBardineo@users.noreply.github.com>
2025-11-23 01:05:29 -0300

    fix: muda fakerJS para PTBR

--- src/utils/mockData.ts
+++ src/utils/mockData.ts
@@ -1,4 +1,4 @@
-import { faker } from '@faker-js/faker';
+import { fakerPT_BR as faker } from '@faker-js/faker';
 import { addMonths, subMonths, startOfMonth, endOfMonth, isBefore } from 'date-fns';
 
 export interface User {
